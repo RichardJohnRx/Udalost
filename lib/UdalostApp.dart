e58d23ca1dfd1e69f987901f0c3d5982436885b6
RichardJohnRx <the.cool.guy492@gmail.com>
2021-03-31 19:56:53 +0200

    Ajout des routes pour la navigation

---
+++ lib/UdalostApp.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:udalost/assets/Palette.dart';
+import 'package:udalost/pages/Accueil.dart';
+import 'package:udalost/pages/EvenementDetailPage.dart';
+import 'package:udalost/pages/EvenementHomePage.dart';
+import 'package:udalost/pages/EvenementMasterPage.dart';
+import 'package:udalost/pages/EvenementSavePage.dart';
+import 'package:udalost/pages/SignIn.dart';
+import 'package:udalost/pages/SignUp.dart';
+
+class UdalostApp extends StatelessWidget {
+  // mettre les objets qu'on va utiliser dans toute l'appli
+  // mettre un utilisateur
+
+  // Create storage to stock the jwt token
+  // final storage = new FlutterSecureStorage();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Udalost',
+      theme: ThemeData(
+        primarySwatch: Palette.purpleNavy,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      routes:{
+        '/': (context) => Accueil(),
+        '/signIn': (context) => SignIn(),
+        '/signUp': (context) => SignUp(),
+        '/evenement/': (context) => EvenementHomePage(),
+        '/evenement/list': (context) => EvenementMaster(),
+        '/evenement/detail': (context) => EvenementDetailPage(),
+        '/evenement/save': (context) => EvenementSavePage(),
+      }
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,10 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:udalost/pages/EvenementDetailPage.dart';
-import 'package:udalost/pages/EvenementHomePage.dart';
-import 'package:udalost/pages/Accueil.dart';
-import 'package:udalost/assets/Palette.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:udalost/UdalostApp.dart';
 
 void main() {
   HttpOverrides.global = new MyHttpOverrides();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // mettre les objets qu'on va utiliser dans toute l'appli
-  // mettre un utilisateur
-
-  // Create storage to stock the jwt token
-  // final storage = new FlutterSecureStorage();
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Udalost',
-      theme: ThemeData(
-        primarySwatch: Palette.purpleNavy,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: EvenementDetailPage(),
-    );
-  }
+  runApp(UdalostApp());
 }
 
 class MyHttpOverrides extends HttpOverrides{
